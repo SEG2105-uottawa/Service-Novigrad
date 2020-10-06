d9ccfcb0f643adcd47aaf43a9f8ee909a7b0b1d0
Garvin Hui <garvinh2001@hotmail.com>
2020-10-06 11:53:29 -0400

    Add verification to user inputs

--- app/src/main/java/com/example/novigrad/RegisterData.java
+++ app/src/main/java/com/example/novigrad/RegisterData.java
@@ -2,9 +2,13 @@ package com.example.novigrad;
 
 import android.util.Patterns;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.rpc.Help;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegisterData {
@@ -13,19 +17,39 @@ public class RegisterData {
 
     String email, password, confirmPassword;
     String firstName, lastName;
+    String roleSelected;
     public RegisterData(RegisterActivity activity) {
         this.firstName = this.getText(activity.firstName);
         this.lastName = this.getText(activity.lastName);
         this.email = this.getText(activity.email);
         this.password = this.getText(activity.password);
         this.confirmPassword = this.getText(activity.confirmPassword);
+        this.roleSelected =  getRadioText(activity.roleSelector);
     }
 
     public boolean isValid(View view) {
-        if (!emailIsValid()) {
+        System.out.println(this.roleSelected);
+        if (!nameIsValid(firstName)) {
+            Helper.snackbar(view, "First Name invalid");
+            return false;
+        } else if (!nameIsValid(lastName)) {
+            Helper.snackbar(view, "Last Name invalid");
+            return false;
+        } else if (!emailIsValid()) {
             Helper.snackbar(view, "Email invalid");
+            return false;
+        } else if (!passwordLengthIsValid()) {
+            Helper.snackbar(view, "Password must be longer than 5 characters");
+            return false;
+        } else if (!passwordIsValid()) {
+            Helper.snackbar(view, "Passwords do not match");
+            return false;
+        } else if (!stringIsValid(this.roleSelected)) {
+            Helper.snackbar(view, "Select a role");
+            return false;
         }
-        return stringIsValid(email) && stringIsValid(password);
+
+        return true;
     }
 
     @Override
@@ -44,6 +68,16 @@ public class RegisterData {
         }
     }
 
+    private String getRadioText(RadioGroup input) {
+        try {
+            RadioButton selected = input.findViewById(input.getCheckedRadioButtonId());
+            return selected.getText().toString();
+        } catch (Exception e) {
+            System.out.println("RadioText bad");
+            return null;
+        }
+    }
+
     private boolean stringIsValid(String s) {
         return (s != null) && (s.length() > 5);
     }
@@ -52,8 +86,20 @@ public class RegisterData {
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             return false;
         }
-
         return true;
     }
 
+    public boolean nameIsValid(String name) {
+        Pattern allowedCharacters = Pattern.compile("[a-zA-z]+");
+        Matcher nameAsMatcher = allowedCharacters.matcher(name);
+        return nameAsMatcher.matches();
+    }
+
+    public boolean passwordLengthIsValid() {
+        return password.length() > 5;
+    }
+
+    public boolean passwordIsValid() {
+        return password.equals(confirmPassword);
+    }
 }
