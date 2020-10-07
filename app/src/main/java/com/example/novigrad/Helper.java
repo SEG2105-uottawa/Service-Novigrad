e8f13b2ce4cdc3898a3b0b52de19635159c136b5
Daniel <danielh839@gmail.com>
2020-10-07 19:20:15 -0400

    Added error handleling for a null view

--- app/src/main/java/com/example/novigrad/Helper.java
+++ app/src/main/java/com/example/novigrad/Helper.java
@@ -6,6 +6,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 public abstract class Helper {
     public static void snackbar(View view, String message) {
-        Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show();
+        if (view != null) {
+            Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show();
+        }
     }
 }
