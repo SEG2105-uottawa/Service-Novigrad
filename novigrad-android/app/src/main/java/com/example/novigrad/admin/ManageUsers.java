6e7c656a7fdec018d3b95a71e1439d6fb58c4722
Garvin Hui <garvinh2001@hotmail.com>
2020-10-28 15:10:59 -0400

    Add tabbing to admin view

---
+++ novigrad-android/app/src/main/java/com/example/novigrad/admin/ManageUsers.java
@@ -0,0 +1,66 @@
+package com.example.novigrad.admin;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.novigrad.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ManageUsers#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ManageUsers extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ManageUsers() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ManageUsers.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ManageUsers newInstance(String param1, String param2) {
+        ManageUsers fragment = new ManageUsers();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_manage_users, container, false);
+    }
+}
---
+++ novigrad-android/app/src/main/java/com/example/novigrad/admin/ServiceEditorActivity.java
@@ -0,0 +1,14 @@
+package com.example.novigrad.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServiceEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_editor);
+    }
+}
