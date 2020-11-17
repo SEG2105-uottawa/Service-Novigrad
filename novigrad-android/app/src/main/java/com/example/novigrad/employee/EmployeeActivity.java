e0bde227b57ba1ea3d0ef1fea66009f1be202c3e
GarvinH <garvinh2001@hotmail.com>
2020-11-16 22:44:22 -0500

    Add new activity for employees after welcome

---
+++ novigrad-android/app/src/main/java/com/example/novigrad/employee/EmployeeActivity.java
@@ -0,0 +1,15 @@
+package com.example.novigrad.employee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.novigrad.R;
+
+public class EmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee);
+    }
+}
