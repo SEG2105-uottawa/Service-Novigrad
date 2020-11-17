c5bb7a7b8c51d60b44d52c7a681228942b4b7506
GarvinH <garvinh2001@hotmail.com>
2020-11-17 13:02:52 -0500

    Add layout for profile editor

---
+++ novigrad-android/app/src/main/java/com/example/novigrad/employee/ProfileEditorAcitivty.java
@@ -0,0 +1,16 @@
+package com.example.novigrad.employee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.novigrad.R;
+
+public class ProfileEditorAcitivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_editor_acitivty);
+    }
+}
