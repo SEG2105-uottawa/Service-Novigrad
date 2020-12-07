511ec5b6016461e992821f917bda6bc4a79d5fb2
GarvinH <garvinh2001@hotmail.com>
2020-12-06 21:13:33 -0500

    Remove unused customer variable in employee

--- novigrad-android/app/src/main/java/com/example/novigrad/domain/Employee.java
+++ novigrad-android/app/src/main/java/com/example/novigrad/domain/Employee.java
@@ -18,20 +18,17 @@ import java.util.ArrayList;
 public class Employee extends User {
     public static String role = "Employee";
     private ArrayList<DocumentReference> services;
-    private ArrayList<DocumentReference> customers;
     private ProfileData profile;
 
     public Employee(String id, String firstName, String lastName, String email) {
         super(id, firstName, lastName, email, role);
         services = new ArrayList<>();
-        customers = new ArrayList<>();
         profile = null;
     }
 
     public Employee(String id, RegisterData registerData) {
         super(id, registerData);
         services = new ArrayList<>();
-        customers = new ArrayList<>();
         profile = null;
 
     }
@@ -41,17 +38,11 @@ public class Employee extends User {
         super(userDocument);
         services = (ArrayList<DocumentReference>) userDocument.get("services");
         services = services == null ? new ArrayList<DocumentReference>() : services;
-        customers = (ArrayList<DocumentReference>) userDocument.get("customers");
-        customers = customers == null ? new ArrayList<DocumentReference>() : customers;
         profile = ProfileData.createProfileDataFromFirebase(userDocument.get("profile"));
     }
 
     public ArrayList<DocumentReference> getServices() {
         return services;
-    }
-
-    public ArrayList<DocumentReference> getCustomers() {
-        return customers;
     }
 
     public ProfileData getProfile() {
